b856ad752e45b033e4442b3eb9dbaa143b767404
vinicius nogueira <v.francisco@riprisma.com>
2023-09-11 12:00:54 -0300

    ajustes

--- js/carrosel.js
+++ js/carrosel.js
@@ -62,6 +62,41 @@ setTimeout(() => {
   // Atualiza a paginação a cada 3 segundos
   setInterval(updatePagination, 3000);
 }, 3000);
+
+// const accordion_item = document.querySelectorAll(".accordion_item");
+
+// accordion_item.forEach((item) => {
+//   const accordion_header_item = item.querySelector(".accordion_header");
+
+//   accordion_header_item.addEventListener("click", () => {
+//     const accordion_content_item = item.querySelector(".accordion_content");
+
+//     const item_actived = document.querySelector(".active");
+
+//     VerifyActive(item, accordion_content_item, item_actived);
+//   });
+// });
+
+// function VerifyActive(item, content, content_actived) {
+//   const icon_item = item.querySelector(".icon");
+//   const icon_item_active = document.querySelectorAll(".icon");
+
+//   icon_item_active.forEach(
+//     (item) => (item.innerHTML = "<i class='fa-solid fa-plus themas' ></i>")
+//   );
+
+//   if (content_actived) {
+//     content_actived.style.height = 0;
+//     content_actived.classList.remove("active");
+//   }
+
+//   if (content !== content_actived) {
+//     icon_item.innerHTML = "<i class='fa-solid fa-minus themas' ></i>";
+//     content.classList.add("active");
+//     content.style.height = content.scrollHeight + 1 + "px";
+//   }
+// }
+
 const accordion_item = document.querySelectorAll(".accordion_item");
 
 accordion_item.forEach((item) => {
@@ -70,7 +105,7 @@ accordion_item.forEach((item) => {
   accordion_header_item.addEventListener("click", () => {
     const accordion_content_item = item.querySelector(".accordion_content");
 
-    const item_actived = document.querySelector(".active");
+    const item_actived = document.querySelector(".active-accordion");
 
     VerifyActive(item, accordion_content_item, item_actived);
   });
@@ -86,12 +121,12 @@ function VerifyActive(item, content, content_actived) {
 
   if (content_actived) {
     content_actived.style.height = 0;
-    content_actived.classList.remove("active");
+    content_actived.classList.remove("active-accordion");
   }
 
   if (content !== content_actived) {
     icon_item.innerHTML = "<i class='fa-solid fa-minus themas' ></i>";
-    content.classList.add("active");
+    content.classList.add("active-accordion");
     content.style.height = content.scrollHeight + 10 + "px";
   }
 }
