7cc0a63fe3759ebc4552392d5010b3ad15caef69
vinicius nogueira <v.francisco@riprisma.com>
2023-09-11 19:36:42 -0300

    contraste internas

--- js/carrosel.js
+++ js/carrosel.js
@@ -40,21 +40,59 @@ var swiper = new Swiper(".mySwiper-equipe", {
   },
 });
 
+// const paginationItems = document.querySelectorAll(".pagination-home-main li");
+// let activeIndex = 0;
+
+// function updatePagination() {
+//   // Remove a classe 'active' de todos os dots
+//   paginationItems.forEach((item) => {
+//     item.classList.remove("active");
+//   });
+
+//   // Define a classe 'active' para o próximo dot
+//   paginationItems[activeIndex].classList.add("active");
+
+//   // Incrementa activeIndex e redefine para 0 quando ultrapassa o número de items
+//   activeIndex = (activeIndex + 1) % paginationItems.length;
+// }
+
+// // Inicializa a paginação após 3 segundos
+// setTimeout(() => {
+//   updatePagination();
+//   // Atualiza a paginação a cada 3 segundos
+//   setInterval(updatePagination, 3000);
+// }, 3000);
+
 const paginationItems = document.querySelectorAll(".pagination-home-main li");
 let activeIndex = 0;
+let isPaused = false; // Variável para controlar a pausa na paginação
 
 function updatePagination() {
-  // Remove a classe 'active' de todos os dots
-  paginationItems.forEach((item) => {
-    item.classList.remove("active");
-  });
+  if (!isPaused) {
+    // Remove a classe 'active' de todos os dots
+    paginationItems.forEach((item) => {
+      item.classList.remove("active");
+    });
 
-  // Define a classe 'active' para o próximo dot
-  paginationItems[activeIndex].classList.add("active");
+    // Define a classe 'active' para o próximo dot
+    paginationItems[activeIndex].classList.add("active");
 
-  // Incrementa activeIndex e redefine para 0 quando ultrapassa o número de items
-  activeIndex = (activeIndex + 1) % paginationItems.length;
+    // Incrementa activeIndex e redefine para 0 quando ultrapassa o número de items
+    activeIndex = (activeIndex + 1) % paginationItems.length;
+  }
 }
+
+// Função para pausar ou retomar a paginação quando o mouse entra/sai de .img-carrosel-home
+function togglePaginationPause() {
+  isPaused = !isPaused;
+}
+
+// Adicionar eventos para pausar quando o mouse entra em .img-carrosel-home
+const imagens = document.querySelectorAll(".img-carrosel-home");
+imagens.forEach((imagem) => {
+  imagem.addEventListener("mouseenter", togglePaginationPause);
+  imagem.addEventListener("mouseleave", togglePaginationPause);
+});
 
 // Inicializa a paginação após 3 segundos
 setTimeout(() => {
@@ -62,40 +100,6 @@ setTimeout(() => {
   // Atualiza a paginação a cada 3 segundos
   setInterval(updatePagination, 3000);
 }, 3000);
-
-// const accordion_item = document.querySelectorAll(".accordion_item");
-
-// accordion_item.forEach((item) => {
-//   const accordion_header_item = item.querySelector(".accordion_header");
-
-//   accordion_header_item.addEventListener("click", () => {
-//     const accordion_content_item = item.querySelector(".accordion_content");
-
-//     const item_actived = document.querySelector(".active");
-
-//     VerifyActive(item, accordion_content_item, item_actived);
-//   });
-// });
-
-// function VerifyActive(item, content, content_actived) {
-//   const icon_item = item.querySelector(".icon");
-//   const icon_item_active = document.querySelectorAll(".icon");
-
-//   icon_item_active.forEach(
-//     (item) => (item.innerHTML = "<i class='fa-solid fa-plus themas' ></i>")
-//   );
-
-//   if (content_actived) {
-//     content_actived.style.height = 0;
-//     content_actived.classList.remove("active");
-//   }
-
-//   if (content !== content_actived) {
-//     icon_item.innerHTML = "<i class='fa-solid fa-minus themas' ></i>";
-//     content.classList.add("active");
-//     content.style.height = content.scrollHeight + 1 + "px";
-//   }
-// }
 
 const accordion_item = document.querySelectorAll(".accordion_item");
 
--- js/carrosel_home.js
+++ js/carrosel_home.js
@@ -1,35 +1,82 @@
+// function adicionarClasseComEfeitoHover() {
+//   const imagens = document.querySelectorAll(".img-carrosel-home");
+//   let index = 0;
+
+//   setInterval(() => {
+//     imagens.forEach((imagem, i) => {
+//       if (i === index) {
+//         imagem.classList.add("hover-effect");
+//       } else {
+//         imagem.classList.remove("hover-effect");
+//       }
+
+//       // Encontra as divs text-carrosel-home e text-carrosel-hover dentro de cada imagem
+//       const textCarroselHome = imagem.querySelector(".text-carrosel-home");
+//       const textCarroselHover = imagem.querySelector(".text-carrosel-hover");
+
+//       // Alterar a visibilidade com base na classe hover-effect
+//       if (imagem.classList.contains("hover-effect")) {
+//         textCarroselHome.style.display = "none";
+//         textCarroselHover.style.display = "flex";
+//         textCarroselHover.style.display = "transition: 0.7s;";
+//       } else {
+//         textCarroselHome.style.display = "flex";
+//         textCarroselHover.style.display = "none";
+//       }
+//     });
+
+//     index = (index + 1) % imagens.length;
+//   }, 3000);
+// }
+
+// adicionarClasseComEfeitoHover();
+
 function adicionarClasseComEfeitoHover() {
   const imagens = document.querySelectorAll(".img-carrosel-home");
   let index = 0;
+  let isPaused = false; // Variável para controlar a pausa
 
-  setInterval(() => {
-    imagens.forEach((imagem, i) => {
-      if (i === index) {
-        imagem.classList.add("hover-effect");
-      } else {
-        imagem.classList.remove("hover-effect");
-      }
+  // Função para pausar ou retomar a execução
+  function togglePause() {
+    isPaused = !isPaused;
+  }
 
-      // Encontra as divs text-carrosel-home e text-carrosel-hover dentro de cada imagem
-      const textCarroselHome = imagem.querySelector(".text-carrosel-home");
-      const textCarroselHover = imagem.querySelector(".text-carrosel-hover");
+  imagens.forEach((imagem) => {
+    imagem.addEventListener("mouseenter", togglePause);
+    imagem.addEventListener("mouseleave", togglePause);
+  });
 
-      // Alterar a visibilidade com base na classe hover-effect
-      if (imagem.classList.contains("hover-effect")) {
-        textCarroselHome.style.display = "none";
-        textCarroselHover.style.display = "flex";
-        textCarroselHover.style.display = "transition: 0.7s;";
-      } else {
-        textCarroselHome.style.display = "flex";
-        textCarroselHover.style.display = "none";
-      }
-    });
+  function nextImage() {
+    if (!isPaused) {
+      imagens.forEach((imagem, i) => {
+        if (i === index) {
+          imagem.classList.add("hover-effect");
+        } else {
+          imagem.classList.remove("hover-effect");
+        }
 
-    index = (index + 1) % imagens.length;
-  }, 3000);
+        const textCarroselHome = imagem.querySelector(".text-carrosel-home");
+        const textCarroselHover = imagem.querySelector(".text-carrosel-hover");
+
+        if (imagem.classList.contains("hover-effect")) {
+          textCarroselHome.style.display = "none";
+          textCarroselHover.style.display = "flex";
+          textCarroselHover.style.transition = "0.7s"; // Corrigido o erro aqui
+        } else {
+          textCarroselHome.style.display = "flex";
+          textCarroselHover.style.display = "none";
+        }
+      });
+
+      index = (index + 1) % imagens.length;
+    }
+  }
+
+  setInterval(nextImage, 3000);
 }
 
 adicionarClasseComEfeitoHover();
+
 
 var swiperCosmos = new Swiper(".mySwiper-cosmos-last", {
   slidesPerView: 1,
