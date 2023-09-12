2decacdb3cc5537d64f2c2f699c7422ae56b343b
vinicius nogueira <v.francisco@riprisma.com>
2023-09-12 20:57:37 -0300

    js carrosel home ok

--- js/carrosel_home.js
+++ js/carrosel_home.js
@@ -31,19 +31,148 @@
 
 // adicionarClasseComEfeitoHover();
 
+// function adicionarClasseComEfeitoHover() {
+//   const imagens = document.querySelectorAll(".img-carrosel-home");
+//   let index = 0;
+//   let isPaused = false; // Variável para controlar a pausa
+
+//   // Função para pausar ou retomar a execução
+//   function togglePause() {
+//     isPaused = !isPaused;
+//   }
+
+//   imagens.forEach((imagem) => {
+//     imagem.addEventListener("mouseenter", togglePause);
+//     imagem.addEventListener("mouseleave", togglePause);
+//   });
+
+//   function nextImage() {
+//     if (!isPaused) {
+//       imagens.forEach((imagem, i) => {
+//         if (i === index) {
+//           imagem.classList.add("hover-effect");
+//         } else {
+//           imagem.classList.remove("hover-effect");
+//         }
+
+//         const textCarroselHome = imagem.querySelector(".text-carrosel-home");
+//         const textCarroselHover = imagem.querySelector(".text-carrosel-hover");
+
+//         if (imagem.classList.contains("hover-effect")) {
+//           textCarroselHome.style.display = "none";
+//           textCarroselHover.style.display = "flex";
+//           textCarroselHover.style.transition = "0.7s"; // Corrigido o erro aqui
+//         } else {
+//           textCarroselHome.style.display = "flex";
+//           textCarroselHover.style.display = "none";
+//         }
+//       });
+
+//       index = (index + 1) % imagens.length;
+//     }
+//   }
+
+//   setInterval(nextImage, 3000);
+// }
+
+// adicionarClasseComEfeitoHover();
+
+//remoção do hover-effect ok
+// function adicionarClasseComEfeitoHover() {
+//   const imagens = document.querySelectorAll(".img-carrosel-home");
+//   const wrapperCosmos = document.querySelector(".wrapper-cosmos"); // Substitua pelo seletor correto
+
+//   let index = 0;
+//   let isPaused = false; // Variável para controlar a pausa
+
+//   // Função para pausar a execução
+//   function pause() {
+//     isPaused = true;
+//     imagens.forEach((imagem) => {
+//       imagem.classList.remove("hover-effect");
+//     });
+//   }
+
+//   // Função para retomar a execução
+//   function resume() {
+//     isPaused = false;
+//   }
+
+//   imagens.forEach((imagem) => {
+//     // Event listener para quando o mouse entra na imagem
+//     imagem.addEventListener("mouseenter", pause);
+
+//     // Event listener para quando o mouse sai da imagem
+//     imagem.addEventListener("mouseleave", resume);
+//   });
+
+//   function nextImage() {
+//     if (!isPaused) {
+//       imagens.forEach((imagem, i) => {
+//         if (i === index) {
+//           imagem.classList.add("hover-effect");
+//         } else {
+//           imagem.classList.remove("hover-effect");
+//         }
+
+//         const textCarroselHome = imagem.querySelector(".text-carrosel-home");
+//         const textCarroselHover = imagem.querySelector(".text-carrosel-hover");
+
+//         if (imagem.classList.contains("hover-effect")) {
+//           textCarroselHome.style.display = "none";
+//           textCarroselHover.style.display = "flex";
+//           textCarroselHover.style.transition = "0.7s";
+//         } else {
+//           textCarroselHome.style.display = "flex";
+//           textCarroselHover.style.display = "none";
+//         }
+//       });
+
+//       index = (index + 1) % imagens.length;
+//     }
+//   }
+
+//   setInterval(nextImage, 3000);
+
+//   // Event listener para quando o mouse entra na div pai
+//   wrapperCosmos.addEventListener("mouseenter", pause);
+
+//   // Event listener para quando o mouse sai da div pai
+//   wrapperCosmos.addEventListener("mouseleave", resume);
+// }
+
+// adicionarClasseComEfeitoHover();
+
 function adicionarClasseComEfeitoHover() {
   const imagens = document.querySelectorAll(".img-carrosel-home");
+  const wrapperCosmos = document.querySelector(".wrapper-cosmos"); // Substitua pelo seletor correto
+
   let index = 0;
   let isPaused = false; // Variável para controlar a pausa
 
-  // Função para pausar ou retomar a execução
-  function togglePause() {
-    isPaused = !isPaused;
+  // Função para pausar a execução
+  function pause() {
+    isPaused = true;
+    imagens.forEach((imagem) => {
+      imagem.classList.remove("hover-effect");
+      const textCarroselHover = imagem.querySelector(".text-carrosel-hover");
+      textCarroselHover.style.display = "none"; // Oculta o text-carrosel-hover
+      const textCarroselHome = imagem.querySelector(".text-carrosel-home");
+      textCarroselHome.style.display = "flex"; // Exibe o text-carrosel-home
+    });
+  }
+
+  // Função para retomar a execução
+  function resume() {
+    isPaused = false;
   }
 
   imagens.forEach((imagem) => {
-    imagem.addEventListener("mouseenter", togglePause);
-    imagem.addEventListener("mouseleave", togglePause);
+    // Event listener para quando o mouse entra na imagem
+    imagem.addEventListener("mouseenter", pause);
+
+    // Event listener para quando o mouse sai da imagem
+    imagem.addEventListener("mouseleave", resume);
   });
 
   function nextImage() {
@@ -61,7 +190,7 @@ function adicionarClasseComEfeitoHover() {
         if (imagem.classList.contains("hover-effect")) {
           textCarroselHome.style.display = "none";
           textCarroselHover.style.display = "flex";
-          textCarroselHover.style.transition = "0.7s"; // Corrigido o erro aqui
+          textCarroselHover.style.transition = "0.7s";
         } else {
           textCarroselHome.style.display = "flex";
           textCarroselHover.style.display = "none";
@@ -73,10 +202,15 @@ function adicionarClasseComEfeitoHover() {
   }
 
   setInterval(nextImage, 3000);
+
+  // Event listener para quando o mouse entra na div pai
+  wrapperCosmos.addEventListener("mouseenter", pause);
+
+  // Event listener para quando o mouse sai da div pai
+  wrapperCosmos.addEventListener("mouseleave", resume);
 }
 
 adicionarClasseComEfeitoHover();
-
 
 var swiperCosmos = new Swiper(".mySwiper-cosmos-last", {
   slidesPerView: 1,
@@ -89,8 +223,6 @@ var swiperCosmos = new Swiper(".mySwiper-cosmos-last", {
     el: ".swiper-pagination-cosmos-last",
   },
 });
-
-
 
 $(document).ready(function () {
   var timeout;
