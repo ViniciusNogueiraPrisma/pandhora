dffa1ecf11e11651ffd1122cbdaa9f5923f07605
vinicius nogueira <v.francisco@riprisma.com>
2023-09-20 17:14:43 -0300

    menu-bar top 129

--- js/carrosel_vertical.js
+++ js/carrosel_vertical.js
@@ -1,7 +1,23 @@
-var swiper = new Swiper(".mySwiper-vertical", {
-    direction: "vertical",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+var swiper = new Swiper(".swiper-slides-fundos", {
+  direction: "vertical",
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiperContainer = document.querySelector(".mySwiper");
+
+swiperContainer.addEventListener("mouseover", function () {
+  swiper.mousewheel.enable();
+});
+
+swiperContainer.addEventListener("mouseout", function () {
+  swiper.mousewheel.disable();
+});
