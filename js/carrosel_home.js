f304fc1b79c628d24573a0f70baecfec1e1ba4f8
vinicius nogueira <v.francisco@riprisma.com>
2023-10-04 12:37:30 -0300

    resp itens

--- js/carrosel_home.js
+++ js/carrosel_home.js
@@ -1,19 +1,3 @@
-// $(document).ready(function () {
-//   var timeout;
-//   $(".nav-item.dropdown").hover(
-//     function () {
-//       clearTimeout(timeout); // limpa o temporizador para evitar a ocultação
-//       $(".dropdown-menu", this).stop(true, true).slideDown("fast");
-//     },
-//     function () {
-//       var $dropdown = $(".dropdown-menu", this);
-//       timeout = setTimeout(function () {
-//         $dropdown.stop(true, true).slideUp("fast");
-//       }, 0);
-//     }
-//   );
-// });
-
 $(document).ready(function () {
   var timeout;
   $(".nav-item.dropdown").hover(
